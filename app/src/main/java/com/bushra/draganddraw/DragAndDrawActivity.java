6907e8a839941efe11d23159ea8ced9e99e49e68
Bushra Al-Mahdi <diamondprincessbushra@gmail.com>
2020-01-19 11:55:17 +0300

    Initial commit

---
+++ app/src/main/java/com/bushra/draganddraw/DragAndDrawActivity.java
@@ -0,0 +1,10 @@
+package com.bushra.draganddraw;
+
+import androidx.fragment.app.Fragment;
+
+public class DragAndDrawActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return DragAndDrawFragment.newInstance();
+    }
+}
